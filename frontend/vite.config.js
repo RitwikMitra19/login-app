7b18560b8218e64af83a8cc2a38f68674f5057c7
Ritwik Mitra <ritwikmitra05@gmail.com>
2025-04-21 19:30:47 +0530

    Changed vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Output directory for production build
+    assetsDir: 'assets', // Directory for static assets
+    emptyOutDir: true, // Clean the output directory before build
+  },
 })
