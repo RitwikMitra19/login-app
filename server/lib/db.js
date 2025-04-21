e4d80420a87fdd72ce4373b3af77d79081ed1361
Ritwik Mitra <ritwikmitra05@gmail.com>
2025-04-21 19:55:04 +0530

    Changing from sql to postgres

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,19 +1,48 @@
-import mysql from "mysql2/promise";
+// import mysql from "mysql2/promise";
 
-let connection;
+// let connection;
+
+// export const connectToDatabase = async () => {
+//   try {
+//     if (!connection) {
+//       connection = await mysql.createConnection({
+//         host: process.env.DB_HOST,
+//         user: process.env.DB_USER,
+//         password: process.env.DB_PASSWORD,
+//         database: process.env.DB_NAME,
+//       });
+//     }
+//     return connection;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+import pg from 'pg';
+const { Pool } = pg;
+
+let pool;
 
 export const connectToDatabase = async () => {
   try {
-    if (!connection) {
-      connection = await mysql.createConnection({
+    if (!pool) {
+      pool = new Pool({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
+        port: process.env.DB_PORT || 5432, // PostgreSQL default port
+        ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
       });
     }
-    return connection;
+    
+    // Test the connection
+    const client = await pool.connect();
+    client.release();
+    
+    return pool;
   } catch (err) {
     console.log(err);
+    throw err;
   }
 };
